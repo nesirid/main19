8e8199adac92ff5bb87fe35fb17630fe4ec1671b
nesirid <nasirid@code.edu.az>
2024-03-12 03:15:56 +0400

    first commit

---
+++ DataListManipulator/DataListManipulator/Models/DataList.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System;
+
+
+public class DataList<T>
+{
+        protected List<T> data;
+
+    public DataList()
+    {
+        data = new List<T>();
+    }
+    public void Add(T item)
+    {
+        data.Add(item);
+    }
+
+    public void Delete(T item)
+    {
+        if (data.Contains(item))
+        {
+            data.Remove(item);
+            Console.WriteLine($"{item} - bu listen pozulub");
+        }
+        else
+        {
+            Console.WriteLine($"{item} - bu listde tapilmadi(null)");
+        }
+
+    }
+    public void Display()
+    {
+        Console.WriteLine("Hal hazirda olan list: ");
+        foreach (T item in data)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public void Clear()
+    {
+        data.Clear();
+        Console.WriteLine("Butun elementler listen silindi.");
+    }
+
+}
+
---
+++ DataListManipulator/DataListManipulator/Program.cs
@@ -0,0 +1,39 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataList<int> intList = new DataList<int>();
+        intList.Add(1);
+        intList.Add(2);
+        intList.Add(3);
+        intList.Add(4);
+        intList.Add(5);
+        intList.Display();
+        intList.Delete(2);
+        intList.Display();
+        intList.Delete(4);
+        intList.Display();
+        intList.Clear();
+        intList.Display();
+
+        Console.WriteLine("________________INT-END_______________");
+
+        DataList<string> stringList = new DataList<string>();
+        stringList.Add("a");
+        stringList.Add("Hello Krab");
+        stringList.Display();
+        stringList.Add("b");
+        stringList.Display();
+        stringList.Delete("b");
+        stringList.Display();
+        stringList.Clear();
+        stringList.Display();
+
+
+
+
+
+
+    }
+}
